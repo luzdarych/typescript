05170a2f71e550138b7da27c3f0c410c16e0fddb
luz Chaparro <luzdarych1031@gmail.com>
2025-11-14 14:27:20 -0500

    finalizacion ejer

--- ICita.ts
+++ ICita.ts
@@ -1,80 +1,122 @@
-interface ICita{
-    paciente:string;
-    fecha:Date;
-    mostrarResumen():string;
-    calcularCosto():number;
+interface ICita {
+    paciente: string;
+    fecha: Date;
+    mostrarResumen(paciente: string, medico:string, fecha: Date, valor: number): string;
+    calcularCosto(): number;
 }
-class Cita implements ICita{
+// clase principal
+class Cita implements ICita {
     private _paciente: string;
-    private _medico:string;
+    private _medico: string;
     private _fecha: Date;
     private _valor: number;
-
-    constructor( paciente:string, medico:string,fecha:Date,valor:number){
+// el costructor es el que recibe los datos
+    constructor(paciente: string, medico: string, fecha: Date, valor: number) {
         this._paciente = paciente;
         this._medico = medico;
         this._fecha = fecha;
         this._valor = valor;
     }
-    get paciente():string{
+    // se usan para poder manejar los datos privados o protegidos 
+    get paciente(): string {
         return this._paciente;
     }
     set paciente(paciente: string) {
         this._paciente = paciente;
     }
-    get medico():string{
+    get medico(): string {
         return this._medico;
     }
     set medico(medico: string) {
         this._medico = medico;
     }
-    get fecha(): Date{
+    get fecha(): Date {
         return this._fecha;
     }
-    set fecha(fecha:Date){
+    set fecha(fecha: Date) {
         this._fecha = fecha;
     }
-    get valor():number{
+    get valor(): number {
         return this._valor;
     }
-    set valor(valor:number){
+    set valor(valor: number) {
         this._valor = valor;
     }
-    public mostrarResumen(): string {
-        return  `el paciente: ${this._paciente},tine cita con el Medico: ${this._medico},en la fecha: ${this._fecha},y tine un valor: ${this._valor} `;
+    // este es el metodo que se va ha reutilizar por eso lo llene entre las ()
+    public mostrarResumen(paciente: string, medico:string, fecha: Date, valor: number): string {
+        return `el paciente: ${paciente},tine cita con el Medico: ${medico},en la fecha: ${fecha},y tine un valor: ${valor} `;
     }
-    public calcularCosto(): number{
+    public calcularCosto(): number {
         return this._valor;
     }
 }
-class CitaGeneral extends Cita{
+// clase hija 
+class CitaGeneral extends Cita {
     Copago: number;
 
-    constructor(paciente:string,medico:string,fecha:Date,valor:number,Copago:number){
-        super(paciente, medico, fecha, valor)
+    constructor(paciente: string, medico: string, fecha: Date, valor: number, Copago: number) {
+        super(paciente, medico, fecha, valor);
         this.Copago = Copago;
     }
+// metodo que estoy reutilizando 
+    reutizarMetMostrarResumen(){    
+        return super.mostrarResumen(this.paciente, this.medico, this.fecha, this.valor + this.Copago);
+    }
 }
-class CitaEspecialista extends Cita{
+class CitaEspecialista extends Cita {
     adicionalEspecialista: number;
 
-    constructor(paciente:string,medico:string,fecha:Date,valor:number,adicionalEspecialista:number){
-        super(paciente,medico,fecha,valor)
+    constructor(paciente: string, medico: string, fecha: Date, valor: number, adicionalEspecialista: number) {
+        super(paciente, medico, fecha, valor)
         this.adicionalEspecialista = adicionalEspecialista;
     }
 
-} 
-// class medico{
-//     nombre:string;
-//     especialidad:string;
-//     tarifaBase:number;
-//     mostrarPerfil():string;
+    reutizarMetMostrarResumen(){
+        return super.mostrarResumen(this.paciente, this.medico, this.fecha, this.valor + this.adicionalEspecialista);
+    }
 
-//     constructor(nombre:string,especialista:string,tarifaBase:number){
-//         this.nombre = nombre;
-//         this.especialidad =especialista;
-//         this.tarifaBase = tarifaBase;
-//     }
+}
+// clase adicional
+class medico {
+    nombre: string;
+    especialidad: string;
+    tarifaBase: number;
+    // mostrarPerfil(): string;
 
+    constructor(nombre: string, especialista: string, tarifaBase: number) {
+        this.nombre = nombre;
+        this.especialidad = especialista;
+        this.tarifaBase = tarifaBase;
+    }
+    public mostrarPerfil(): string {
+        return `el medico ${this.nombre} de la especialidad ${this.especialidad} maneja una tarifa de ${this.tarifaBase}`
+    }
 
-// }
+}
+// es donde creo los objetos 
+const cita1 = new Cita("Erika Bautista", "Yudy Cogaria", new Date, 7000);
+const citaGeneral1 = new CitaGeneral("Santiago Castillo", "Jose Velez", new Date, 7000, 4200);
+const CitaEspecialista1 = new CitaEspecialista("Maria Plazas", "Juan Puerto", new Date, 7000, 10000);
+const medico1 = new medico("Jose Cipagauta","Psiquiatra",10000);
+
+// es el arreglo
+let miArregloCitas = [];
+let miArregloCitasGenEsp = [];
+let miArregloMedicos = [];
+miArregloCitas.push(cita1);
+miArregloCitasGenEsp.push(citaGeneral1);
+miArregloCitasGenEsp.push(CitaEspecialista1);
+miArregloMedicos.push(medico1);
+
+// aca recorro el arreglo
+miArregloCitas.forEach(element => {
+    console.log(element.mostrarResumen(cita1.paciente, cita1.medico, cita1.fecha, cita1.valor));
+});
+
+miArregloCitasGenEsp.forEach(element => {
+    console.log(element.reutizarMetMostrarResumen());
+});
+
+miArregloMedicos.forEach(element => {
+    console.log(element.mostrarPerfil());
+});
